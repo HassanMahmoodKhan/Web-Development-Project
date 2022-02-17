e7d911b3809eaae218a35e2cb95ea3d76d5f1043
PriyeshBhalala105 <priyeshbhalala105@gmail.com>
2022-02-17 13:44:32 -0500

    Database updated with other functionality

--- app.py
+++ app.py
@@ -17,15 +17,55 @@ class  trip_data (db.Model):
     id = db.Column(db.Integer, primary_key= True)
     origin = db.Column(db.Text)
     destination = db.Column(db.Text)
+    date = db.Column(db.Text)
     
     
 
-    def __init__(self, origin, destination):
+    def __init__(self, origin, destination, date):
         self.origin = origin
         self.destination = destination
+        self.date = date
         
     def __repr__(self):
-        return (f"{self.origin}+{self.destination}")
+        return (f"Train from {self.origin} to {self.destination} is available on {self.date}")
+
+################################################################################
+################# Do not comment out below code ################################
+################################################################################
+
+
+# db.create_all()
+
+
+# trip_1 = trip_data('Toronto','Montreal', 'Date')
+# trip_2 = trip_data('Toronto','Ottawa', 'Date')
+# trip_3 = trip_data('Toronto','Vancouver', 'Date')
+# trip_4 = trip_data('Toronto','Mississauga', 'Date')
+# trip_5 = trip_data('Montreal','Toronto', 'Date')
+# trip_6 = trip_data('Montreal','Ottawa', 'Date')
+# trip_7 = trip_data('Montreal','Vancouver', 'Date')
+# trip_8 = trip_data('Montreal','Mississauga', 'Date')
+# trip_9 = trip_data('Ottawa','Toronto', 'Date')
+# trip_10 = trip_data('Ottawa','Montreal', 'Date')
+# trip_11 = trip_data('Ottawa','Vancouver', 'Date')
+# trip_12 = trip_data('Ottawa','Mississauga', 'Date')
+# trip_13 = trip_data('Vancouver','Toronto', 'Date')
+# trip_14 = trip_data('Vancouver','Montreal', 'Date')
+# trip_15 = trip_data('Vancouver','Ottawa', 'Date')
+# trip_16 = trip_data('Vancouver','Mississauga', 'Date')
+# trip_17 = trip_data('Mississauga','Toronto', 'Date')
+# trip_18 = trip_data('Mississauga','Montreal', 'Date')
+# trip_19 = trip_data('Mississauga','Ottawa', 'Date')
+# trip_20 = trip_data('Mississauga','Vancouver', 'Date')
+
+
+# db.session.add_all([trip_1, trip_2, trip_3, trip_4, trip_5, trip_6, trip_7, trip_8, trip_9, trip_10, trip_11, trip_12, trip_13, trip_14,trip_15, trip_16, trip_17, trip_18, trip_19, trip_20])
+                     
+# db.session.commit()   
+
+print (trip_data.query.filter_by(origin='Toronto', destination='Montreal').all())
+# # print (trip_data.query.all())
+
 
 
 ################################################################################
@@ -111,7 +151,7 @@ def trip():
     if origin=='Mississauga' and destination=='vancouver':
         return (str(trp20[0]))
     else:
-        return ('sorry!')
+        return ('Sorry! Invalid Input.')
 
 
     # return(str(select))
